4ff2bd6980773ba2d409731449a51ed200bfdfd2
polonskiybogdan <polonskiybogdan@gmail.com>
2019-04-15 00:19:53 +0300

    first try one module

---
+++ src/main/java/com/trainingportalgroupone/trainingportal/TrainingportalApplication.java
@@ -0,0 +1,13 @@
+package com.trainingportalgroupone.trainingportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingportalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingportalApplication.class, args);
+    }
+
+}
