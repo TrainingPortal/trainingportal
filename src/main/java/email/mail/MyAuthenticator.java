9d43be3b466529dbedf917d6b6219442a16f9ca0
Lova <lova2010@ukr.net>
2019-04-15 18:40:48 +0300

    Mail class -> New Structure + Test for mail

---
+++ src/main/java/email/mail/MyAuthenticator.java
@@ -0,0 +1,21 @@
+package email.mail;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+//Here we get login and password from user and set private fields
+class MyAuthenticator extends Authenticator {
+    private String user;
+    private String password;
+
+    MyAuthenticator(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    public PasswordAuthentication getPasswordAuthentication() {
+        String user = this.user;
+        String password = this.password;
+        return new PasswordAuthentication(user, password);
+    }
+}
