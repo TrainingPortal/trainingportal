a76313eb20bce3dc869c082e33dd393a7c498110
MaxBurba <maksburba@gmail.com>
2019-04-20 16:08:35 +0300

    Improved register and login activities

--- src/main/java/trainingportal/mapper/UserMapper.java
+++ src/main/java/trainingportal/mapper/UserMapper.java
@@ -9,18 +9,20 @@ import java.sql.SQLException;
 public class UserMapper implements RowMapper<User> {
  
     public static final String BASE_SQL
-            = "Select u.User_Id, u.User_Name, u.User_Email, u.Encryted_Password, u.Enabled From App_User u ";
- 
+            = "SELECT u.userId, u.name, u.email, u.password, u.enabled, u.token, u.roleId FROM users u ";
+    public static final String PASSWORD_SQL
+            = "SELECT u.userId, u.name, u.email, u.enabled, u.token, u.roleId FROM users u ";
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
  
-        Long userId = rs.getLong("User_Id");
-        String userName = rs.getString("User_Name");
-        String userEmail = rs.getString("User_Email");
-        String encryptedPassword = rs.getString("Encryted_Password");
-        int enabled = rs.getInt("Enabled");
+        Long userId = rs.getLong("userId");
+        String userName = rs.getString("name");
+        String userEmail = rs.getString("email");
+        String password = rs.getString("password");
+        int enabled = rs.getInt("enabled");
+        String token = rs.getString("token");
+        Long roleId = rs.getLong("roleId");
 
-        return new User(userId, userName,userEmail, encryptedPassword, enabled);
+        return new User(userId, userName,userEmail, password, enabled, token, roleId);
     }
- 
 }
