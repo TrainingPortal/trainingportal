eaa19c1422786b1053677ba467eda764c25e7297
Lova <lova2010@ukr.net>
2019-05-11 23:29:27 +0300

    New Repotrs class

--- src/main/java/trainingportal/reports/controller/DataController.java
+++ src/main/java/trainingportal/reports/controller/DataController.java
@@ -11,7 +11,7 @@ import trainingportal.model.Attendance;
 import trainingportal.model.Course;
 import trainingportal.model.Role;
 import trainingportal.model.User;
-import trainingportal.reports.service.DataService;
+import trainingportal.reports.reportslist.Reports;
 import trainingportal.service.AttendanceType;
 import trainingportal.service.CourseService;
 import trainingportal.service.UserService;
@@ -23,8 +23,6 @@ import java.util.List;
 @Controller
 public class DataController {
 
-    @Autowired
-    private DataService dataService;
 
     @Autowired
     private Download download;
@@ -37,6 +35,9 @@ public class DataController {
 
     @Autowired
     private AttendanceType attendanceType;
+
+    @Autowired
+    private Reports report;
 
     @RequestMapping(value = "data/download", method = RequestMethod.GET)
     public ModelAndView download(@NotNull ModelAndView model){ return model; }
@@ -97,7 +98,7 @@ public class DataController {
     @RequestMapping(value = "data/downloadTrainer/{trainerId}", method = RequestMethod.GET)
     public ResponseEntity downloadTrainerFile(@PathVariable("trainerId") Long trainerId){
 
-        if (createNewTrainerReport(trainerId)){
+        if(report.createNewTrainerReport(trainerId)){
             return download.downloadFile("Trainer.xlsx");
         }
         return null;
@@ -106,7 +107,7 @@ public class DataController {
     @RequestMapping(value = "data/downloadLevel/{levelName}", method = RequestMethod.GET)
     public ResponseEntity downloadLevelFile(@PathVariable("levelName") String levelName){
 
-        if (createNewLevelReport(levelName)){
+        if (report.createNewLevelReport(levelName)){
             return download.downloadFile("Level.xlsx");
         }
         return null;
@@ -115,65 +116,9 @@ public class DataController {
     @RequestMapping(value = "data/downloadAttendance/{attendanceId}", method = RequestMethod.GET)
     public ResponseEntity downloadAttendanceFile(@PathVariable("attendanceId") Long attendanceId){
 
-        if (createNewAttendanceReport(attendanceId)){
+        if (report.createNewAttendanceReport(attendanceId)){
             return download.downloadFile("Attendance.xlsx");
         }
         return null;
-    }
-
-    private boolean createNewTrainerReport(Long trainerId){
-
-        List list = new ArrayList();
-        list.add("Trainer Name");
-        list.add("Course Name");
-        list.add("Course Level");
-        list.add("Course Status");
-
-        //Select
-        String sql = "SELECT DISTINCT users.name as \"Trainer Name\", course.name as \"Course Name\", course.course_level as \"Course Level\", course_status.name_status as \"Course Status\" \n" +
-                "FROM course INNER JOIN course_status ON course.course_status_id = course_status.id INNER JOIN users ON course.trainer_id = users.role_id WHERE users.user_id =  " + trainerId;
-
-        List<List> courses = dataService.getMultiFieldsFromTables(list, sql,"Trainer","table");
-        return true;
-    }
-
-    private boolean createNewLevelReport(String levelName){
-
-        List list = new ArrayList();
-        list.add("User Name");
-        list.add("Role");
-        list.add("Email");
-        list.add("Course Name");
-        list.add("Group Name");
-
-        //Choose by users who is managers or employee that have availability enter to course and course level = to ......
-        String sql = "SELECT DISTINCT users.name as \"User Name\", email as \"Email\", course.name as \"Course Name\", course.course_level as \"Course Level\" \n" +
-                "FROM users INNER JOIN desired_period ON users.user_id = desired_period.user_id INNER JOIN course ON desired_period.course_id = course.course_id\n" +
-                "WHERE ( users.role_id = 2 OR users.role_id = 4 ) AND course.course_level = " + "\'" + levelName + "\'";
-
-        List<List> courses = dataService.getMultiFieldsFromTables(list, sql,"Level","table");
-        return true;
-    }
-
-    private boolean createNewAttendanceReport(Long attendanceId){
-
-        List list = new ArrayList();
-        list.add("User Name");
-        list.add("Lesson Date");
-        list.add("Lesson Name");
-        list.add("Group Name");
-        list.add("Course Name");
-        list.add("Status");
-
-        //Choose by attendance for only active or stopped course where attendance type = to ......
-        String sql = "SELECT users.name as \"User Name\", schedule.date_lesson as \"Lesson Date\", lesson.lesson_name as \"Lesson Name\", type, groups.name as \"Group Name\", course.name as \"Course Name\", course_status.name_status as \"Status\"  \n" +
-                "FROM attendance INNER JOIN users ON attendance.user_id = users.user_id INNER JOIN schedule ON attendance.schedule_id = schedule.id \n" +
-                "LEFT OUTER JOIN attendance_type ON attendance.type_id = attendance_type.id \n" +
-                "INNER JOIN lesson ON schedule.lesson_id = lesson.lesson_id LEFT OUTER JOIN groups ON schedule.group_id = groups.id\n" +
-                "LEFT OUTER JOIN course ON groups.course_id = course.course_id INNER JOIN course_status ON course.course_status_id = course_status.id\n" +
-                "WHERE ( course_status.id = 1 OR course_status.id = 3 ) AND attendance_type.id = " + attendanceId;
-
-        List<List> courses = dataService.getMultiFieldsFromTables(list, sql,"Attendance","table");
-        return true;
     }
 }
