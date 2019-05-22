c0da0a0b1bee4ae955f084dbf37e42b3619805cb
Bogadan <45152317+PolonskiyBogdan@users.noreply.github.com>
2019-05-22 18:45:54 +0300

    Revert "Fix for lesson (#78)" (#80)
    This reverts commit c3e0c1c4fc765fea09618ad6275daa9271edeaef.

--- src/main/java/trainingportal/controller/UserController.java
+++ src/main/java/trainingportal/controller/UserController.java
@@ -24,8 +24,8 @@ public class UserController {
     public Sender mailSender;
 
 
-//      private static final String SERVER_NAME = "http://localhost:8080";
-    private static final String SERVER_NAME = "http://onetrainingportal-env.w6ev2hpcfm.eu-west-2.elasticbeanstalk.com";
+      private static final String SERVER_NAME = "http://localhost:8080";
+//    private static final String SERVER_NAME = "http://onetrainingportal-env.w6ev2hpcfm.eu-west-2.elasticbeanstalk.com";
 
     @Autowired
     private UserService userService;
